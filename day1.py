c86a2c0d0b711e020029c308085d751fa37cb9fb
git178 <kchadha5@gmail.com>
2021-03-26 19:40:22 +0530

    Day 1

---
+++ day1.py
@@ -0,0 +1,12 @@
+# Numbers with decimal parts are called floats
+# Numbers without the decimal parts are called int or integers
+
+print(10 / 10)# 10 divided by 10
+print(10 * 10)# 10 times 10
+print(10 // 10)# 10 times 10 without the decimal part
+print(10 ** 10)# 10 to the power of 10
+print(10 + 10)# 10 plus 10
+print(10 - 10)# 10 minus 10
+print(11 % 10)# the remainder of 11 divided by 10
+print(11 == 10)# Checks whether 11 is equal to 10 or not, print True or False based on outcome
+print((10 // 4) ** 3)# Anything in brackets will have higher priorty just like BODMAS
