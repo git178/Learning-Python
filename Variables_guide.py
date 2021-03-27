39249e4f5b41db95d9b03fbb3b5060d5be15c159
samrath2007 <samrath1324@gmail.com>
2021-03-27 22:12:46 +0530

    Added instructor's explanation

---
+++ Variables_guide.py
@@ -0,0 +1,23 @@
+# Variables are like containers
+# Except when you "ask" for the "container" you get the value inside the container
+
+x = 1
+# The variable x now has a value of 1
+
+x = 37890124
+# The variable x got overwritten and is now 37890124
+
+x = 10 + 5
+# The variable x got overwritten again and is now 15
+
+y = 41.23
+# A new variable y has got a value of 41.23
+
+x = y + 14
+# Since y acts as a number, x would be 41.23 + 14, which is 55.23
+
+y = y + x
+# The variable y can also be use to overwrite itself
+# Since y will now be
+# 41.23 + 55.23
+# That means y will now be 96.46
