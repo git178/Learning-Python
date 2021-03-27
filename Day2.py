08eaf04948c59772b522ec5c1621a52e1490f42b
git178 <kchadha5@gmail.com>
2021-03-27 21:46:26 +0530

    Today we learnt variables
    You can change the value of a variable and the computer will work with the new value.

---
+++ Day2.py
@@ -0,0 +1,18 @@
+x = 110 / 11
+print(x)
+x = x + 90
+print(x)
+x = x / 10
+print(x)
+x = (x - 8) * 25
+print(x)
+x = x // 356
+print(x)
+x = (x * 105) / 3
+print(x)
+x = x  % 100
+print(x)
+x = x +  2000
+print(x)
+x = x - 1
+print(x)
