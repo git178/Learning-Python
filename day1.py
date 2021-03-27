36142804a1868966f499baadbb37f2beb4231b06
Samrath Chadha <69572668+samrath2007@users.noreply.github.com>
2021-03-27 22:13:22 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -20,5 +20,5 @@ print(10 + 10)# 10 plus 10
 print(10 - 10)# 10 minus 10
 print(11 % 10)# the remainder of 11 divided by 10
 print(11 == 10)# Checks whether 11 is equal to 10 or not, print True or False based on outcome
-print((10 // 4) ** 3)# Anyth /.ing in brackets will have higher priorty just like BODMAS
+print((10 // 4) ** 3)# Anything in brackets will have higher priorty just like BODMAS
 print(10 == 10)
