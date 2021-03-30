f2ffa05e4af38ecaf4433a39458b2ca094ae5b1c
git178 <kchadha5@gmail.com>
2021-03-30 12:30:29 +0530

    Learnt strings AND indexing

--- Day2.py
+++ Day2.py
@@ -16,3 +16,6 @@ x = x +  2000
 print(x)
 x = x - 1
 print(x)
+# A Variable cannot start with a number or have any special characters other than underscore. 
+# It can have any number of letters- small or capital.
+# However, all capital letters is not a good practice.
---
+++ Day3.py
@@ -0,0 +1,15 @@
+buddy= "extremely loyal and cute"
+print(len(buddy))
+print(buddy[0])
+print(buddy[0:10])
+print(buddy[0:])
+print(buddy[:15])
+print(buddy[:])
+# len gives you the length or number of characters of the string.
+# So (len(buddy)) when printed = 24 as even spaces are counted.
+# [] these brackets help to dissect the components of the string.
+# so (buddy[0]) = e
+# so (buddy[0:10]) = extremely
+# so (buddy[0:]) = extremely loyal and cute
+# so (buddy[:15]) = extremely loyal
+# so (buddy[:]) = extremely loyal and cute
